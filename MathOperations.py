7e043268e0130fe328afa9b3ded3c29e7628c1c0
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-24 21:37:23 +0500

    Square Root if number is positive

--- MathOperations.py
+++ MathOperations.py
@@ -1,15 +1,22 @@
-num = float(input("Enter any number: "))
+# num = float(input("Enter any number: "))
 
-sq = num * num
-print("Square of given number is :", sq)
+# sq = num * num
+# print("Square of given number is :", sq)
 
-sr = num ** 0.5
-print("Square root of given number is :", sr)
+# sr = num ** 0.5
+# print("Square root of given number is :", sr)
+
+# import math
+# sqroot = math.sqrt(num)
+# print("Square root of given number is :", sqroot)
+
+# square= math.pow(num, 2)
+# print("Square of given number is :", square)
 
 import math
-sqroot = math.sqrt(num)
-print("Square root of given number is :", sqroot)
-
-square= math.pow(num, 2)
-print("Square of given number is :", square)
-# num = float(input("Enter any number: "))
+num = float(input("Enter any number: "))
+if (num > 0):
+    s_root = math.sqrt(num)
+    print("Square root of given number is :", s_root)
+else:
+    print("Square root of negative number is not possible")
