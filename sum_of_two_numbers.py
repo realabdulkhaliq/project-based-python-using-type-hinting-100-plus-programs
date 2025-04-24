f237fcb482a8544b91e52075ce40a71d52d70821
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-24 14:27:37 +0500

    Sum of numbers input from user

--- sum_of_two_numbers.py
+++ sum_of_two_numbers.py
@@ -6,3 +6,11 @@ print("The sum of two numbers is :", num1 + num2)
 # sum = num1 + num2
 # print("The sum of two numbers is :", sum)
 
+# num3 = int(input("Enter first number: "))
+# num4 = int(input("Enter second number: "))
+
+# print("The sum of two numbers is :", num3 + num4)
+
+# sum = num3 + num4
+# print("The sum of two numbers is :", sum)
+
