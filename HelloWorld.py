2bd4ad06b8406eae50c4139d93c13ffb93e0320c
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-26 13:31:37 +0500

    Print End Pram

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,7 @@ print("Hello World")
 print("Hello\t World")
 print("Hello\n World")
 print("Hello\b World")
+
+print("Hello World", end="")
+print("Hello World", end=" ")
+print("Hello World", end="\n")
