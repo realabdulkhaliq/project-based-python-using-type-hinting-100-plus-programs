91a9b2fbc4ff577c6221e7149a8f0aeece639a51
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-26 13:31:51 +0500

    Print Sep Pram

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,3 +6,8 @@ print("Hello\b World")
 print("Hello World", end="")
 print("Hello World", end=" ")
 print("Hello World", end="\n")
+
+print("Hello", "World", sep="\t")
+print("Hello", "World", sep="\n")
+print("Hello", "World", sep="....", end="")
+print("Hello", "World", sep="....")
