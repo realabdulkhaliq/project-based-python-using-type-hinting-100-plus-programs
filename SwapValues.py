b1cda702c7f46565eaec0088d8ca4b7095b6c95b
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-27 07:57:06 +0500

    Swap 2 Values

---
+++ SwapValues.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swapping, first number is:", num1)
+print("After swapping, second number is:", num2)
+
