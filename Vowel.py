f33f406db7549e2d0c11cc51d1be53704cf0ab1a
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-28 15:05:52 +0500

    Character is Vowel or not

---
+++ Vowel.py
@@ -0,0 +1,5 @@
+character = input("Enter a character: ")
+if character in ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'):
+    print(character, "is a Vowel")
+else:
+    print(character, "is a consonant")
