a2b0ee766b6673370ed9bf378a13b7230f8be94f
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-29 07:18:18 +0500

    Prime Numbers in a range

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -1,11 +1,24 @@
-num = int(input("Enter a number: "))
-if num == 1:
-    print("1 is neither prime nor composite")
-if num > 1:
-    for i in range(2, num):
-        if (num % i) == 0:
-            print(num, "is not a prime number")
-            break
-    else:
-        print(num, "is a prime number")
+# num = int(input("Enter a number: "))
+# if num == 1:
+#     print("1 is neither prime nor composite")
+# if num > 1:
+#     for i in range(2, num):
+#         if (num % i) == 0:
+#             print(num, "is not a prime number")
+#             break
+#     else:
+#         print(num, "is a prime number")
 
+
+# Prime Numbers in a range
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num, end=" ")
+print("are prime numbers between", lower, "and", upper)
