fca37b3157ec671084c26d12329af9d48020d7fd
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-29 07:23:09 +0500

    Largest of three numbers Using list comprehension

--- LargestOfThree.py
+++ LargestOfThree.py
@@ -29,3 +29,8 @@ print("Largest number is:", largest)
 # Using lambda function
 largest = (lambda x, y, z: x if (x > y and x > z) else y if (y > x and y > z) else z)(num1, num2, num3)
 print("Largest number is:", largest)
+
+# Using list comprehension
+largest = [num1, num2, num3]
+largest.sort(reverse=True)
+print("Largest number is:", largest[0])
