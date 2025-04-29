5076873019be510badeec7bc472b72f737bb80d3
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-04-29 07:21:22 +0500

    Largest of three numbers Using lambda function

--- LargestOfThree.py
+++ LargestOfThree.py
@@ -25,3 +25,7 @@ else:
 # Using max function
 largest = max(num1, num2, num3)
 print("Largest number is:", largest)
+
+# Using lambda function
+largest = (lambda x, y, z: x if (x > y and x > z) else y if (y > x and y > z) else z)(num1, num2, num3)
+print("Largest number is:", largest)
