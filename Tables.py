f4a9959e26fa5a1bab438d98cf1c9d94401d8522
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-01 08:57:06 +0500

    print table of any number using while loop in python

--- Tables.py
+++ Tables.py
@@ -4,3 +4,8 @@ num = int(input("Enter a number whose table did you want to print: "))
 for i in range(1, 11):
     print(num, "x", i, "=", num * i)
 
+# While loop to print the table of the number
+i = 1
+while (i > 11):
+    print(num, "x", i, "=", num * i)
+    i += 1
