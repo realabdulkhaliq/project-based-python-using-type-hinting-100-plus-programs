d73145780de8c8049a7daec32f1ef53caecc7e8e
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-02 10:48:40 +0500

    This code calculates the power of 2 for a given range of numbers using for loop in python

---
+++ PowerOfTwo.py
@@ -0,0 +1,11 @@
+# This code calculates the power of 2 for a given range of numbers.
+lower = int(input("Enter lower range: "))
+upper = int(input("Enter upper range: "))
+
+for i in range(lower, upper + 1):
+    if i > 0:
+        print( 2 ** i, end=" ")
+    else:
+        print("Power of 2 is not possible for negative numbers")
+        break
+
