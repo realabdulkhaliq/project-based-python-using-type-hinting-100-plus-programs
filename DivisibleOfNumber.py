7af9198844c16a9394b10de8f24148dd9288bed9
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-05 06:54:11 +0500

    Check one number is divisible by other in python using lambda function

--- DivisibleOfNumber.py
+++ DivisibleOfNumber.py
@@ -8,3 +8,9 @@ elif num2 % num1 == 0:
 else:
     print(num1, "is not divisible by", num2)
 
+
+numlist = [ 33, 39, 77, 65, 90, 100, 135, 150, 230, 270, 300 ]
+num = int(input("Enter a number: "))
+
+result = list(filter(lambda x: x % num == 0, numlist))
+print("The numbers divisible by", num, "are:", result)
