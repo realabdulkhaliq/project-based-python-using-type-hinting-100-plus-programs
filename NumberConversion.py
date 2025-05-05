38ab91f29ff21a99320d4b477b3f8dac3f41f267
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-05 07:06:06 +0500

    Veriable Names Changed and now error removed

--- NumberConversion.py
+++ NumberConversion.py
@@ -13,27 +13,27 @@ print("Decimal:", decimal, "in octal is", format(decimal, 'o'))
 print("Decimal:", decimal, "in hexadecimal is", format(decimal, 'x'))
 print("Decimal:", decimal, "in binary is", format(decimal, 'X'))
 
-binary = int(input("Enter a binary number: "), 2)
-print("decimal is", binary)
+binary_to_decimal = int(input("Enter a binary number: "), 2)
+print("decimal is", binary_to_decimal)
 
-octal = int(input("Enter an octal number: "), 8)
-print("Decimal is", octal)
+octal_to_decimal = int(input("Enter an octal number: "), 8)
+print("Decimal is", octal_to_decimal)
 
-hexadecimal = int(input("Enter a hexadecimal number: "), 16)
-print("Decimal is", hexadecimal)
+hexadecimal_to_decimal = int(input("Enter a hexadecimal number: "), 16)
+print("Decimal is", hexadecimal_to_decimal)
 
 
-binary_str = input("Enter a binary number: ")
-decimal_num = int(binary_str, 2)
-print(f"Decimal value: of {binary_str} is {decimal_num}")
+# binary_str = input("Enter a binary number: ")
+# decimal_num = int(binary_str, 2)
+# print(f"Decimal value: of {binary_str} is {decimal_num}")
 
-# Manual conversion
-binary_str = input("Enter a binary number: ")
-decimal_num = 0
+# # Manual conversion
+# binary_str = input("Enter a binary number: ")
+# decimal_num = 0
 
-for i, digit in enumerate(reversed(binary_str)):
-    if digit == '1':
-        decimal_num += 2 ** i
+# for i, digit in enumerate(reversed(binary_str)):
+#     if digit == '1':
+#         decimal_num += 2 ** i
 
-print(f"Decimal value: {decimal_num}")
+# print(f"Decimal value: {decimal_num}")
 
