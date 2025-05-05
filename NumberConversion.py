6c6924ee85f8a9a6f91f57a6030dab5aa1a65081
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-05 06:57:01 +0500

    Decimal to Binary Octal and Haxa Decimal Conversion using Format in Python

--- NumberConversion.py
+++ NumberConversion.py
@@ -8,3 +8,8 @@ print("Decimal:", decimal, "in binary is", binary[2:])
 print("Decimal:", decimal, "in octal is", octal[2:])
 print("Decimal:", decimal, "in hexadecimal is", hexadecimal[2:])
 
+print("Decimal:", decimal, "in binary is", format(decimal, 'b'))
+print("Decimal:", decimal, "in octal is", format(decimal, 'o'))
+print("Decimal:", decimal, "in hexadecimal is", format(decimal, 'x'))
+print("Decimal:", decimal, "in binary is", format(decimal, 'X'))
+
