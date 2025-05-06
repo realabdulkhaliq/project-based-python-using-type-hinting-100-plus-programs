4922176fc10d58f2c56775102ffdb966d3e22233
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-06 07:08:05 +0500

    Mini Calculator + - x / in Python using try except for Division by zero & Invalid operation

--- MiniCalculator.py
+++ MiniCalculator.py
@@ -1,23 +1,42 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-operation = input("Enter operation (+, -, *, /): ")
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+# operation = input("Enter operation (+, -, *, /): ")
 
-if operation == "+":
-    result = num1 + num2
-elif operation == "-":
-    result = num1 - num2
-elif operation == "*":
-    result = num1 * num2
-elif operation == "/":
-    if num2 != 0:
-        result = num1 / num2
+# if operation == "+":
+#     result = num1 + num2
+# elif operation == "-":
+#     result = num1 - num2
+# elif operation == "*":
+#     result = num1 * num2
+# elif operation == "/":
+#     if num2 != 0:
+#         result = num1 / num2
+#     else:
+#         result = "Error! Division by zero."
+# else:
+#     result = "Invalid operation!"
+
+# print("Result:", result)
+
+
+try:
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    operation = input("Enter operation (+, -, *, /): ")
+    if operation == "+":
+        print(num1 + num2)
+    elif operation == "-":
+        print(num1 - num2)
+    elif operation == "*":
+        print(num1 * num2)
+    elif operation == "/":
+        if num2 != 0:
+            print(num1 / num2)
+        else:
+            print("Error! Division by zero is not possible.")
     else:
-        result = "Error! Division by zero."
-else:
-    result = "Invalid operation!"
+        print("Invalid operation!")
 
-print("Result:", result)
-
-
-
+except ValueError:
+    print("Invalid input. Please enter a numeric value.")
 
