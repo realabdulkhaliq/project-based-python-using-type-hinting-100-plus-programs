e2a007a8ad3f904ef4ebca9850cbc6b634e79933
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-08 11:49:01 +0500

    Check any number is Armstrong Number or Not

--- ArmstrongNumber.py
+++ ArmstrongNumber.py
@@ -18,3 +18,21 @@ else:
 # An Armstrong number is a number that is equal to the sum of its own digits raised 
 # to the power of the number of digits. 
 # For example, 153 is an Armstrong number because 1^3 + 5^3 + 3^3 = 153.
+
+
+order = len(str(num))
+sum = 0
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** order
+    temp //= 10
+
+if num == sum:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
+
+
+# Armstrong numbers are numbers that are equal to the sum of their own digits raised to the power of the number of digits.
+
