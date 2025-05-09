a341456e8e2dabea79fe78ea5bcf3d029c7c1651
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-09 09:33:11 +0500

    Python Fabonacci Sequence using if else

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -9,3 +9,12 @@ for i in range(0, n):
     a , b = b , c       # a= 1 , b = 1         a = 1 , b = 2
 
 
+if n == 0:
+    print(a)
+else:
+    print(a, end=" ")
+    print(b, end=" ")
+    for i in range(2, n):
+        c = a + b
+        a , b = b , c
+        print(c, end=" ")
