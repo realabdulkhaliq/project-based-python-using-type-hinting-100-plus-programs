aa29510f0ef0a3bb0765fb5f6dfbc7891709b6c7
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-11 19:27:47 +0500

    Decimal to binary using recursion in python taking input from user

--- DeciToBinaryRecursion.py
+++ DeciToBinaryRecursion.py
@@ -4,3 +4,12 @@ def DeciToBinary(n):
         DeciToBinary(n // 2)
     print(n % 2, end='')
 
+# Get user input
+num = int(input("Enter a decimal number: "))
+if num < 0:
+    print("Please enter a positive number")
+else:
+    print("The binary representation of", num, "is:" , end=' ')
+    DeciToBinary(num) # Here due to positional argument error
+    # print("The binary representation of", num, "is:" , DeciToBinary(num)) # error
+    # print(DeciToBinary(num)) # error
