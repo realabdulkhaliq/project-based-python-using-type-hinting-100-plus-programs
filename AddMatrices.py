d8f611a13b7c7a587791e9065ab13ea6b34fb3b3
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-14 08:57:03 +0500

    Python: Print the result in matrix C in Matrix form

--- AddMatrices.py
+++ AddMatrices.py
@@ -15,3 +15,11 @@ C = [[0,0,0],
 for i in range(len(A)):
     for j in range(len(A[0])):
         C[i][j] = A[i][j] + B[i][j]
+
+# Print the result
+print("Resultant matrix:")
+for r in C:
+    print(r)
+
+
+# The above code adds two matrices A and B and stores the result in matrix C.
