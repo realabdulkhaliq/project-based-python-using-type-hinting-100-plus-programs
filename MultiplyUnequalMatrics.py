b631303aa8ca68ef5fe1aafe65396c388356c533
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-18 12:54:26 +0500

    Python: Itration of rows and columns for multiplication

--- MultiplyUnequalMatrics.py
+++ MultiplyUnequalMatrics.py
@@ -10,3 +10,13 @@ R = [[0,0,0,0],
      [0,0,0,0],
      [0,0,0,0]]
 
+# Matrix Multiplication
+# Iterate through rows of A
+for i in range(len(A)):
+    # Iterate through columns of B
+    for j in range(len(B[0])):
+        # Iterate through rows of B
+        for k in range(len(B)):
+            R[i][j] += A[i][k] * B[k][j]
+
+
