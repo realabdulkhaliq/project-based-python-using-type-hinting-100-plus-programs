0c4635b06a23fb2a4cb4d5c41c0896d0c57b1e22
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-19 07:28:25 +0500

    Python: String Sort

--- StringSort.py
+++ StringSort.py
@@ -2,6 +2,8 @@ input_string = input("Enter a string: ")
 
 new_string = input_string.split() # Split the string into list of words
 
+for i in range(len(new_string)):
+    new_string[i] = new_string[i].lower()
 
 new_string.sort() # Sort the list of words
 
