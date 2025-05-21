0fb70baec44acf21c7deb1e0d17aa32676274213
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-21 07:20:51 +0500

    Function to remove punctuation from a string

--- RemovePunctuation.py
+++ RemovePunctuation.py
@@ -11,13 +11,13 @@ print("String without punctuation:", new_string)
 
 # Function to remove punctuation from a string
 
-def remove_punctuation(input_string):
-    # Create a translation table to remove punctuation
-    translator = str.maketrans('', '', punctuation)
-    # Use the translate method to remove punctuation
-    return input_string.translate(translator)
-# Example usage
-input_string = "Hello, World! This is a test string with punctuation."
-output_string = remove_punctuation(input_string)
-print("Original string:", input_string)
-print("String without punctuation:", output_string)
+# def remove_punctuation(input_string):
+#     # Create a translation table to remove punctuation
+#     translator = str.maketrans('', '', punctuation)
+#     # Use the translate method to remove punctuation
+#     return input_string.translate(translator)
+# # Example usage
+# input_string = "Hello, World! This is a test string with punctuation."
+# output_string = remove_punctuation(input_string)
+# print("Original string:", input_string)
+# print("String without punctuation:", output_string)
