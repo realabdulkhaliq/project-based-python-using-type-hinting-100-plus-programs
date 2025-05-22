2b5e71601bd0c7e7158cb18ba890e1f79b55d3fb
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-22 11:57:55 +0500

    Python: Check if the string is a palindrome

--- StringPalindrome.py
+++ StringPalindrome.py
@@ -2,3 +2,10 @@ get_string = input("Enter a string: ")
 new_string = get_string.replace(" ", "").lower()
 reverse_string = new_string[::-1]
 
+# ============
+# # Check if the string is a palindrome
+# if new_string == reverse_string:
+#     print(f"{get_string} is a palindrome.")
+# else:
+#     print(f"{get_string} is not a palindrome.")
+
