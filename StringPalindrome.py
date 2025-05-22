2b0a29e72b159f40cbb67497576f559c3919237e
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-22 12:02:36 +0500

    Python: is_palindrome function commented

--- StringPalindrome.py
+++ StringPalindrome.py
@@ -24,14 +24,14 @@ reverse_string = new_string[::-1]
 #     print(f"{get_string} is not a palindrome.")
 
 
-# ============
-def is_palindrome(s):
-    # Remove spaces and convert to lowercase
-    s = s.replace(" ", "").lower()
-    # Check if the string is equal to its reverse
-    return s == s[::-1]
+# # ============
+# def is_palindrome(s):
+#     # Remove spaces and convert to lowercase
+#     s = s.replace(" ", "").lower()
+#     # Check if the string is equal to its reverse
+#     return s == s[::-1]
 
-if is_palindrome(get_string):
-    print(f"{get_string} is a palindrome.")
-else:
-    print(f"{get_string} is not a palindrome.")
+# if is_palindrome(get_string):
+#     print(f"{get_string} is a palindrome.")
+# else:
+#     print(f"{get_string} is not a palindrome.")
