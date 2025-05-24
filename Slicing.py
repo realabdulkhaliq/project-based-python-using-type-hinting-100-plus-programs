74d3e44c4115574160a5c4027cc0607a6d9cccb8
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-24 10:35:01 +0500

    Python: Out of index slicing

--- Slicing.py
+++ Slicing.py
@@ -1,3 +1,6 @@
 mylist: list = ['abu bakar', 'umar', 'usman', 'ali']
 
 print(mylist[0:2])  # ['abu bakar', 'umar']
+print(mylist[0:3])  # ['abu bakar', 'umar', 'usman']
+print(mylist[0:5])  # ['abu bakar', 'umar', 'usman', 'ali']
+print(mylist[0:6])  # ['abu bakar', 'umar', 'usman', 'ali']
