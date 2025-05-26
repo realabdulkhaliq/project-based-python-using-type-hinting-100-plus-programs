970aa892f927fa07d93e8bb754b3acb0e68642a3
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-26 06:20:40 +0500

    Python: Find time elapsed to run a given program

---
+++ TimeElapsed.py
@@ -0,0 +1,10 @@
+import time
+
+starting_time = time.time()
+
+print("Hello, World!")
+# This code prints "Hello, World!" and measures the time taken to execute this print statement.
+
+ending_time = time.time()
+print(f"Time taken to execute the print statement: {ending_time - starting_time:.4f} seconds")
+
