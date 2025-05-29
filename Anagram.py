b1d657e9e495f51f9d0f7e07300d5da9c75d7269
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-29 08:25:09 +0500

    Python: Check if strings are anagram using len(str1) == len(str2) and sorted(str1) == sorted(str2)

--- Anagram.py
+++ Anagram.py
@@ -1,7 +1,7 @@
 str1 = input("Enter the first string: ")
 str2 = input("Enter the second string: ")
 
-if len(str1) == len(str2):
+if len(str1) == len(str2) and sorted(str1) == sorted(str2):
     print(f"{str1} and {str2} are anagrams.")
 else:
     print(f"{str1} and {str2} are not anagrams.")
