f347f4091d49ba407d8ff15b60d68e3165fb5852
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-05-31 06:21:52 +0500

    Python: Find power of any base number whose exponent is given using while loop

--- PowerofANumber.py
+++ PowerofANumber.py
@@ -2,7 +2,13 @@ base_number = int(input("Enter the base number: "))
 exponent = int(input("Enter the exponent: "))
 
 result = 1
-for i in range(exponent):
-    result *= base_number
-print(f"{base_number} raised to the power of {exponent} is: {result}")
+# for i in range(exponent):
+#     result *= base_number
+# print(f"{base_number} raised to the power of {exponent} is: {result}")
 
+# Alternative method using a while loop
+while exponent > 0:
+    result *= base_number
+    exponent -= 1
+
+print(f"{base_number} raised to the power of {exponent} is: {result}")
