45adda6e36d3accffab7a74a6e17bac03c11169e
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-06-03 18:03:26 +0500

    Python:  This code reverses any given number using a while loop.

--- ReverseAnyNumber.py
+++ ReverseAnyNumber.py
@@ -8,3 +8,14 @@ print("Reversed number:", rn)
 
 # This code reverses any given number.
 
+print("=====")
+while num != 0:
+    digit = num % 10
+    rn = rn * 10 + digit
+    num //= 10
+
+print("Reversed number using while loop:", rn)
+# This code reverses any given number using a while loop.
+
+
+
