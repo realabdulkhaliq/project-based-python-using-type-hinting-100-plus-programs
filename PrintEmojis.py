875c88c8d594c02df9e4041af17412a1dab23eae
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-06-04 11:57:56 +0500

    Python: Heavy Exclamation Mark Emoji

--- PrintEmojis.py
+++ PrintEmojis.py
@@ -4,4 +4,4 @@ print(b'Heart \xE2\x9D\xA4'.decode('utf-8')) # Red Heart Emoji
 print(b'Check \xE2\x9C\x94'.decode('utf-8')) # Check Mark Emoji
 print(b'Cross \xE2\x9C\x96'.decode('utf-8')) # Cross Mark Emoji
 print(b'Question \xE2\x9D\x93'.decode('utf-8')) # Question Mark Emoji
-
+print(b'Exclamation \xE2\x9D\x97'.decode('utf-8')) # Heavy Exclamation Mark Emoji
