180e07ca03542f861162e866cbc8b6c78940ea39
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-06-04 11:56:08 +0500

    Python: Red Heart Emoji

--- PrintEmojis.py
+++ PrintEmojis.py
@@ -1,2 +1,3 @@
 print(b'Smile \xF0\x9F\x98\x81'.decode('utf-8')) # Grinning Face Emoji
 print(b'Sad \xF0\x9F\x98\x94'.decode('utf-8')) # Disappointed Face Emoji
+print(b'Heart \xE2\x9D\xA4'.decode('utf-8')) # Red Heart Emoji
