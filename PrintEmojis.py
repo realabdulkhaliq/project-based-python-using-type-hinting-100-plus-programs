f5715ad2a9b92fda1dc86a4f7ca76cb462a5c604
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-06-04 11:56:29 +0500

    Python: Check Mark Emoji

--- PrintEmojis.py
+++ PrintEmojis.py
@@ -1,3 +1,4 @@
 print(b'Smile \xF0\x9F\x98\x81'.decode('utf-8')) # Grinning Face Emoji
 print(b'Sad \xF0\x9F\x98\x94'.decode('utf-8')) # Disappointed Face Emoji
 print(b'Heart \xE2\x9D\xA4'.decode('utf-8')) # Red Heart Emoji
+print(b'Check \xE2\x9C\x94'.decode('utf-8')) # Check Mark Emoji
