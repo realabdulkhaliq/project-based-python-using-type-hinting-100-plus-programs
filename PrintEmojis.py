0762594efb80f3af3c9b15b0dd2d2b0d69963ffb
ABDUL KHALIQ <thenewagedevelopers@gmail.com>
2025-06-04 11:57:02 +0500

    Python: Cross Mark Emoji

--- PrintEmojis.py
+++ PrintEmojis.py
@@ -2,3 +2,6 @@ print(b'Smile \xF0\x9F\x98\x81'.decode('utf-8')) # Grinning Face Emoji
 print(b'Sad \xF0\x9F\x98\x94'.decode('utf-8')) # Disappointed Face Emoji
 print(b'Heart \xE2\x9D\xA4'.decode('utf-8')) # Red Heart Emoji
 print(b'Check \xE2\x9C\x94'.decode('utf-8')) # Check Mark Emoji
+print(b'Cross \xE2\x9C\x96'.decode('utf-8')) # Cross Mark Emoji
+print(b'Question \xE2\x9D\x93'.decode('utf-8')) # Question Mark Emoji
+print(b'Exclamation \xE2\x9D\x97'.decode('utf-8')) # Heavy Exclamation Mark Emoji
